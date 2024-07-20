44b0d97adb680010a28ea2ca528d290c616cfd1d
Etienne Camenen <etienne.camenen@gmail.com>
2024-07-20 13:01:03 +0200

    Improve plot_bar
    * Add error bar, abs, geom point, sample_size, label custom

--- R/plot_bar.R
+++ R/plot_bar.R
@@ -34,18 +34,30 @@
 #' @return A ggplot object.
 #' @export
 plot_bar <- function(
-    x = NULL,
-    title = NULL,
-    width_title = 20,
-    colour = c("blue", "gray", "#cd5b45"),
-    color_title = "black",
-    cex = 1,
-    cex_main = cex * 30,
-    digits = 0,
-    n_max = 100,
-    ratio = 5,
-    threshold = 2,
-    hjust_title = 0) {
+        x = NULL,
+        title = NULL,
+        width_text = 30,
+        width_title = 20,
+        colour = c("#cd5b45", "gray", "blue"),
+        color_title = "black",
+        colour_text = "white",
+        cex = 1,
+        cex_main = cex * 30,
+        digits = 0,
+        n_max = 100,
+        ratio = 5,
+        threshold = 2,
+        hjust_title = 0.5,
+        hjust_ratio = 0.25,
+        sample_size = NULL,
+        label_x = "percent",
+        label_y = "none",
+        sort = "asc",
+        error = NULL,
+        abs = FALSE,
+        geom = "bar",
+        nudge_x = 0
+        ) {
     if (is.null(title)) {
         title <- deparse(substitute(x))
     }
@@ -56,20 +68,47 @@ plot_bar <- function(
     }
     df <- (
         df0 %>%
-            mutate(name = rownames(.)) %>%
-            arrange(desc(val)) %>%
-            mutate(order = rev(seq(nrow(df0))))
-    ) %>%
+            mutate(name = rownames(.) %>% str_wrap(width_text)) %>%
+            mutate(order0 = seq(nrow(.)))
+    )
+    if (!is.null(error)) {
+        df <- df %>%
+            mutate(error = error)
+    }
+    if (isTRUE(abs)) {
+        func <- function(x) abs(x)
+    } else {
+        func <- function(x) x
+    }
+    if (sort == "asc") {
+        df <- df %>%
+            arrange(desc(func(val)))
+    } else {
+        df <- df %>%
+            arrange(func(val))
+    }
+    df <- df %>% mutate(order = rev(seq(nrow(.)))) %>%
         head(n_max) %>%
         set_rownames(.$name)
-    p <- ggplot(df, aes(order, val, fill = order)) +
+    p <- ggplot(df, aes(order, val, fill = order, color = order)) +
         theme_minimal()
+    if (length(colour) == nrow(df0)) {
+        colour <- colour[pull(df, "order0")]
+    }
     colors <- colorRampPalette(colour)(length(p$data$val))
     y_lab <- p$data$val / 2
-    x_lab <- round(p$data$val, digits)
-    x_lab[p$data$val < threshold] <- ""
-    p +
-        geom_bar(stat = "identity") +
+    if (geom == "bar") {
+        p <- p +
+            geom_bar(stat = "identity")
+    } else {
+        p <- p +
+            geom_point(size = cex * 3)
+    }
+    if (!is.null(error)) {
+        p <- p +
+            geom_errorbar(aes(ymin = val - error, ymax = val + error), width = 0.3)
+    }
+    p <- p +
         expand_limits(y = max(p$data$val) + max(p$data$val) / ratio) +
         coord_flip() +
         scale_x_continuous(breaks = df$order, labels = rownames(df)) +
@@ -77,11 +116,45 @@ plot_bar <- function(
             breaks = pretty_breaks(n = 4),
             labels = label_number_auto()
         ) +
-        labs(title = str_wrap(title, width_title)) +
-        geom_text(
-            aes(color = I("white"), y = y_lab, label = x_lab),
+        labs(title = str_wrap(title, width_title))
+    if (is.null(sample_size)) {
+        sample_size <- nrow(df0)
+    }
+
+    x_lab <- (p$data$val / sample_size * 100) %>%
+        round(digits) %>%
+        paste0("%")
+    # x_lab <- ifelse(x_lab < threshold, "", paste0(x_lab, "%"))
+    x_lab0 <- round(p$data$val, digits)
+    x_lab0[abs(p$data$val) < threshold] <- ""
+    if (label_x == "percent") {
+        lab_x <- x_lab
+    } else {
+        lab_x <- x_lab0
+    }
+    if (label_x != "none") {
+        p <- p + geom_text(
+            color = colour_text,
+            aes(y = y_lab, label = lab_x),
             size = cex * 7
-        ) +
+        )
+    }
+    if (label_y == "percent") {
+        lab_y <- x_lab
+    } else {
+        lab_y <- x_lab0
+    }
+    if (label_y != "none") {
+        p <- p + geom_text(
+            aes(label =  lab_y),
+            hjust = ifelse(p$data$val < 0, 1 + hjust_ratio, 0 - hjust_ratio),
+            vjust = 0.5,
+            size = cex * 7,
+            color = colors,
+            nudge_x = nudge_x
+        )
+    }
+    p +
         theme(
             axis.text.y = element_text(
                 size = cex * 20,
@@ -107,6 +180,7 @@ plot_bar <- function(
         ) %>%
         suppressWarnings() +
         theme(legend.position = "none") +
-        scale_fill_gradientn(colours = rev(colors), na.value = "black") %>%
+        scale_fill_gradientn(colours = rev(colors), na.value = "black") +
+        scale_color_gradientn(colours = rev(colors), na.value = "black") %>%
         suppressWarnings()
 }
